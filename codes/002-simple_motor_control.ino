7aab6b0b3722de365d752770670a214d7a516e03
Braz Amorim <104170924+BrazAC@users.noreply.github.com>
2023-04-14 16:54:20 -0300

    Update 002-simple_motor_control.ino

--- codes/002-simple_motor_control.ino
+++ codes/002-simple_motor_control.ino
@@ -1,25 +1,47 @@
 //======Motor A======//
-int ENA = 2; //Velocidade
-int IN1 = 3; 
-int IN2 = 4;
+int VA = 6; //Velocidade A
+int IN1 = 7; 
+int IN2 = 8;
 //======Motor B======//
-/*
-int ENA = 2; //Velocidade
-int IN1 = 3; 
-int IN2 = 4;
-*/
+int VB = 11; //Velocidade B
+int IN3 = 12; 
+int IN4 = 13;
 void setup() 
 {
+  //===Configurando gpios
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(VA, OUTPUT);
+  pinMode(VB, OUTPUT);
+
+  //===Ajustando velocidade
+  analogWrite(VA, 63);
+  analogWrite(VB, 63);
+
+  delay(1000);
+
+  //===Ligando motor A
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  //===Ligando motor B
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  delay(7000);
+
+  //===Desligando motor A
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  //===Desligando motor B
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
 void loop() 
 {
-  delay(1000);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  delay(1000);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
+
+
+  
 }
